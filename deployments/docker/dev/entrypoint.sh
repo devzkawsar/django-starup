420806a3b37f5b70259dddbd9f03bd321b1b2567
devzkawsar <zul.kawsar@polygontech.xyz>
2024-05-27 19:10:07 +0600

    change

--- deployments/docker/dev/entrypoint.sh
+++ deployments/docker/dev/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 set -e
 python manage.py migrate --noinput
-# python manage.py collectstatic --noinput
-# python manage.py runserver
+python manage.py collectstatic --noinput
 
 count=$(ls -1 *.env 2>/dev/null | wc -l)
 if [ "$count" != 0 ]; then
@@ -11,6 +10,7 @@ fi
 
 set -a; source .env; set +a
 
+# python manage.py runserver
 # python manage.py migrate
 
 gunicorn --config gunicorn.conf.py config.wsgi:application
