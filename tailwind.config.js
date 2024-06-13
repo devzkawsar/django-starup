fe7a6c716be6a0dd79a3971584dd2f7cfe1ac0a0
devzkawsar <zul.kawsar@polygontech.xyz>
2024-06-13 20:30:20 +0600

    duisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,27 @@
 module.exports = {
   content: [
     'apps/**/*.html',
+    './node_modules/daisyui/**/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          '500' : "#22c55e"
+        },
+        gray: {
+          '400' : "#9ca3af",
+          '500': "#6b7280"
+        }
+      },
+      borderColor: {
+        gray: {
+          '300': "#d1d5db",
+          '400' : "#9ca3af",
+          '500': "#6b7280"
+        }
+      }
+    },
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
@@ -22,11 +40,30 @@ module.exports = {
         900: '#14532d',
       },
     },
+    letterSpacing: {
+      tighter: '-.5em',
+      tight: '-.25em',
+      normal: '0',
+      wide: '.25em',
+      wider: '.5em',
+      widest: '.75em',
+    },
     fontFamily: {
       'sans': ['Helvetica'],
     }
   },
   plugins: [
-  ]
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
 }
 
